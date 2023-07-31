10423b8d251f8d06798599a69758c254f612b0a8
Jonathan Bogasky <boganaut@gmail.com>
2023-07-31 17:50:21 -0500

    we're instantiating data with references!

--- src/data/init_data.py
+++ src/data/init_data.py
@@ -1,3 +1,6 @@
-from data.models import Character
+from data.models import Character, Room, Mobile, MobileType
 
+Room.create_room('This is the deepest darkest void.')
+MobileType.add_type('monster')
+Mobile.create_mobile('a big stinky green goblin', 'monster', 1)
 Character.create_character('Rha', '1')
--- src/data/models.py
+++ src/data/models.py
@@ -1,12 +1,14 @@
 import logging
 import hashlib
+from typing import Optional
 from sqlalchemy import ForeignKey
 from sqlalchemy.types import Integer, String
 from sqlalchemy.orm import (DeclarativeBase,
                             Mapped,
                             mapped_column,
                             validates,
-                            Session)
+                            Session,
+                            relationship)
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import (MultipleResultsFound,
                                 NoResultFound)
@@ -17,41 +19,71 @@ from exceptions import (LoginError,
 class Base(DeclarativeBase):
     pass
 
-class Item(Base):
-    __tablename__ = 'item'
+class MudObject(Base):
+    __tablename__ = 'mud_object'
     id: Mapped[int] = mapped_column(primary_key=True)
+
+class Item(MudObject):
+    __tablename__ = 'item'
+    id: Mapped[int] = mapped_column(ForeignKey('mud_object.id'), primary_key=True)
     short_desc: Mapped[str] = mapped_column(String(32), nullable=False)
-    long_desc: Mapped[str] = mapped_column(String(255))
+    long_desc: Mapped[Optional[str]] = mapped_column(String(255))
     item_type: Mapped[int] = mapped_column(ForeignKey('item_type.id'))
-    room: Mapped[int] = mapped_column(ForeignKey('room.id'))
+    parent: Mapped[int] = mapped_column(ForeignKey('mud_object.id'))
+
+    __mapper_args__ = {
+        'inherit_condition': (id == MudObject.id)
+    }
 
 class ItemType(Base):
     __tablename__ = 'item_type'
     id: Mapped[int] = mapped_column(primary_key=True)
     type_name: Mapped[str] = mapped_column(String(32), unique=True)
 
-class Mobile(Base):
+class Mobile(MudObject):
     __tablename__ = 'mobile'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey('mud_object.id'), primary_key=True)
     short_desc: Mapped[str] = mapped_column(String(32), nullable=False)
-    long_desc: Mapped[str] = mapped_column(String(255))
+    long_desc: Mapped[Optional[str]] = mapped_column(String(255))
     mobile_type: Mapped[int] = mapped_column(ForeignKey('mobile_type.id'))
-    room: Mapped[int] = mapped_column(ForeignKey('room.id'))
+    room_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
+
+    @classmethod
+    def create_mobile(cls, short_desc, mobile_type, room_id, long_desc=None):
+        with Session(ENGINE) as session:
+            type_id = session.query(MobileType.id).where(MobileType.type_name == mobile_type).one()[0]
+            session.add(Mobile(short_desc=short_desc,
+                               mobile_type=type_id,
+                               room_id=room_id,
+                               long_desc=long_desc))
+            session.commit()
 
 class MobileType(Base):
     __tablename__ = 'mobile_type'
     id: Mapped[int] = mapped_column(primary_key=True)
     type_name: Mapped[str] = mapped_column(String(32), unique=True)
 
+    @classmethod
+    def add_type(cls, type_name):
+        with Session(ENGINE) as session:
+            session.add(MobileType(type_name=type_name))
+            session.commit()
+
 class Direction(Base):
     __tablename__ = 'direction'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     dir_name: Mapped[str] = mapped_column(String(10), unique=True)
 
-class Room(Base):
+class Room(MudObject):
     __tablename__ = 'room'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey('mud_object.id'), primary_key=True)
     desc: Mapped[str] = mapped_column(String(255))
+
+    @classmethod
+    def create_room(cls, desc):
+        with Session(ENGINE) as session:
+            session.add(Room(desc=desc))
+            session.commit()
 
 class RoomConnection(Base):
     __tablename__ = 'room_connection'
@@ -60,11 +92,12 @@ class RoomConnection(Base):
     room_id: Mapped[str] = mapped_column(ForeignKey('room.id'))
     destination_id: Mapped[str] = mapped_column(ForeignKey('room.id'))
 
-class Character(Base):
+class Character(MudObject):
     __tablename__ = 'character'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey('mud_object.id'), primary_key=True)
     name: Mapped[str] = mapped_column(String(16), unique=True)
     account_hash: Mapped[str] = mapped_column(String(64))
+    room_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
 
     @classmethod
     def validate_account(cls, character, hash):
@@ -77,10 +110,10 @@ class Character(Base):
         return hashlib.sha256(hash.encode('utf-8')).hexdigest() == account_hash
     
     @classmethod
-    def create_character(cls, name, hash):
+    def create_character(cls, name, hash, room=1):
         with Session(ENGINE) as session:
             try:
-                session.add(Character(name=name, account_hash=hash))
+                session.add(Character(name=name, account_hash=hash, room_id=room))
                 session.commit()
             except IntegrityError as e:
                 logging.exception(e)
