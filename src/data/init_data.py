bc2328f328431bdc08cc5060cf6a8d3e986eb49c
Jonathan Bogasky <boganaut@gmail.com>
2023-08-02 20:21:10 -0500

    working on room connection data modeling

--- src/data/init_data.py
+++ src/data/init_data.py
@@ -1,6 +1,14 @@
-from data.models import Character, Room, Mobile, MobileType
+from data.models import Character, Room, Mobile, MobileType, RoomConnection, Direction
+import logging
 
-Room.create_room('This is the deepest darkest void.')
+r1 = Room.create_room('This is the deepest darkest void.')
+r2 = Room.create_room('You\'ve gone into the light.')
+logging.info(r1)
+logging.info(r2)
+Direction.create_direction('east', 'west')
+Direction.create_direction('west', 'east')
+RoomConnection.create_bidirectional_connection(r1, r2, 'east') 
 MobileType.add_type('monster')
 Mobile.create_mobile('a big stinky green goblin', 'monster', 1)
 Character.create_character('Rha', '1')
+logging.info(r1.exits())
--- src/data/models.py
+++ src/data/models.py
@@ -1,13 +1,13 @@
 import logging
 import hashlib
-from typing import Optional
-from sqlalchemy import ForeignKey
+from typing import Optional, List
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.types import Integer, String
 from sqlalchemy.orm import (DeclarativeBase,
                             Mapped,
                             mapped_column,
                             validates,
-                            Session,
+                            sessionmaker,
                             relationship)
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import (MultipleResultsFound,
@@ -15,6 +15,8 @@ from sqlalchemy.orm.exc import (MultipleResultsFound,
 from config import ENGINE
 from exceptions import (LoginError,
                            CharacterExists)
+
+Session = sessionmaker(ENGINE, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
@@ -28,7 +30,7 @@ class Item(MudObject):
     id: Mapped[int] = mapped_column(ForeignKey('mud_object.id'), primary_key=True)
     short_desc: Mapped[str] = mapped_column(String(32), nullable=False)
     long_desc: Mapped[Optional[str]] = mapped_column(String(255))
-    item_type: Mapped[int] = mapped_column(ForeignKey('item_type.id'))
+    item_type: Mapped[int] = mapped_column(ForeignKey('item_type.name'))
     parent: Mapped[int] = mapped_column(ForeignKey('mud_object.id'))
 
     __mapper_args__ = {
@@ -37,48 +39,57 @@ class Item(MudObject):
 
 class ItemType(Base):
     __tablename__ = 'item_type'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    type_name: Mapped[str] = mapped_column(String(32), unique=True)
+    name: Mapped[str] = mapped_column(String(32), primary_key=True)
 
 class Mobile(MudObject):
     __tablename__ = 'mobile'
     id: Mapped[int] = mapped_column(ForeignKey('mud_object.id'), primary_key=True)
     short_desc: Mapped[str] = mapped_column(String(32), nullable=False)
     long_desc: Mapped[Optional[str]] = mapped_column(String(255))
-    mobile_type: Mapped[int] = mapped_column(ForeignKey('mobile_type.id'))
+    mobile_type: Mapped[int] = mapped_column(ForeignKey('mobile_type.name'))
     room_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
 
     @classmethod
     def create_mobile(cls, short_desc, mobile_type, room_id, long_desc=None):
-        with Session(ENGINE) as session:
-            type_id = session.query(MobileType.id).where(MobileType.type_name == mobile_type).one()[0]
-            session.add(Mobile(short_desc=short_desc,
-                               mobile_type=type_id,
-                               room_id=room_id,
-                               long_desc=long_desc))
+        with Session() as session:
+            mobile = Mobile(short_desc=short_desc,
+                            mobile_type=mobile_type,
+                            room_id=room_id,
+                            long_desc=long_desc)
+            session.add(mobile)
             session.commit()
+        return mobile
 
     @classmethod
     def delete_mobile(cls, id):
-        with Session(ENGINE) as session:
+        with Session() as session:
             session.query(Mobile).where(Mobile.id == id).delete()
             session.commit()
 
 class MobileType(Base):
     __tablename__ = 'mobile_type'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    type_name: Mapped[str] = mapped_column(String(32), unique=True)
+    name: Mapped[str] = mapped_column(String(32), primary_key=True)
 
     @classmethod
     def add_type(cls, type_name):
-        with Session(ENGINE) as session:
-            session.add(MobileType(type_name=type_name))
+        with Session() as session:
+            mobile_type = MobileType(name=type_name)
+            session.add(mobile_type)
             session.commit()
+        return mobile_type
 
 class Direction(Base):
     __tablename__ = 'direction'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    dir_name: Mapped[str] = mapped_column(String(10), unique=True)
+    name: Mapped[str] = mapped_column(String(10), primary_key=True)
+    inverse: Mapped[str] = mapped_column(String(10))
+
+    @classmethod
+    def create_direction(cls, direction, inverse):
+        with Session() as session:
+            direction = Direction(name=direction, inverse=inverse)
+            session.add(direction)
+            session.commit()
+        return direction
 
 class Room(MudObject):
     __tablename__ = 'room'
@@ -87,16 +98,50 @@ class Room(MudObject):
 
     @classmethod
     def create_room(cls, desc):
-        with Session(ENGINE) as session:
-            session.add(Room(desc=desc))
+        with Session() as session:
+            room = Room(desc=desc)
+            session.add(room)
             session.commit()
+        return room
+    
+    def exits(self):
+        with Session() as session:
+            exits = session.query(RoomConnection.direction).where(RoomConnection.room_id == self.id).all()
+        return [exit for exit in exits]
 
 class RoomConnection(Base):
     __tablename__ = 'room_connection'
     id: Mapped[int] = mapped_column(primary_key=True)
-    direction: Mapped[str] = mapped_column(ForeignKey('direction.dir_name'))
-    room_id: Mapped[str] = mapped_column(ForeignKey('room.id'))
-    destination_id: Mapped[str] = mapped_column(ForeignKey('room.id'))
+    room_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
+    destination_id: Mapped[int] = mapped_column(ForeignKey('room.id'))
+    direction: Mapped[str] = mapped_column(ForeignKey('direction.name'))
+
+    UniqueConstraint('room_id', 'destination_id')
+
+    @classmethod
+    def create_bidirectional_connection(cls, room, destination, direction):
+        with Session() as session:
+            inverse = session.query(Direction.inverse).where(Direction.name == direction).one()[0]
+            connections = [ 
+                RoomConnection(room_id=room.id,
+                               destination_id=destination.id,
+                               direction=direction),
+                RoomConnection(room_id=destination.id,
+                               destination_id=room.id,
+                               direction=inverse)
+            ]
+            session.add_all(connections)
+            session.commit()
+        return connections
+    
+    def create_unidirectional_connection(cls, room, destination, direction):
+        with Session() as session:
+            connection = RoomConnection(room_id=room.id,
+                                        destination_id=destination.id,
+                                        direction=direction)
+            session.add(connection)
+            session.commit()
+        return connection
 
 class Character(MudObject):
     __tablename__ = 'character'
@@ -107,7 +152,7 @@ class Character(MudObject):
 
     @classmethod
     def validate_account(cls, character, hash):
-        with Session(ENGINE) as session:
+        with Session() as session:
             try:
                 account_hash = session.query(Character.account_hash).where(Character.name == character).one()[0]
             except (NoResultFound, MultipleResultsFound) as e:
@@ -117,13 +162,15 @@ class Character(MudObject):
     
     @classmethod
     def create_character(cls, name, hash, room=1):
-        with Session(ENGINE) as session:
+        with Session() as session:
             try:
-                session.add(Character(name=name, account_hash=hash, room_id=room))
+                character = Character(name=name, account_hash=hash, room_id=room)
+                session.add(character)
                 session.commit()
             except IntegrityError as e:
                 logging.exception(e)
                 raise CharacterExists from e
+        return character
         
     @validates('account_hash')
     def _hash_password(self, key, hash):
