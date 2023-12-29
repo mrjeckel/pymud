971c661f59f4ff362fe71988ae4edcd0647b5f82
mrjecekl <jonathanbogasky@gmail.com>
2023-12-29 16:46:56 -0600

    dox

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# pymud
+# What is this?
+A framework for building multi-user dungeons(MUDs). It currently handles:
+- client connections
+- global events
+- command parsing
+- data persistence
+
+# Getting Started
+## Requirements
+- docker
+- docker-compose
+
+```
+# Add user to docker group
+~/PyMUD$: sudo gpasswd -a $USER docker
+
+# Start the dev environment
+~/PyMUD$: make dev
+
+# Connect
+~/PyMUD$: telnet localhost 5000
+```
+
+# Future Work
+- Finish validation of targetting
+- Fix room descriptions
+- Build commands
+- Containers
+- Character Creation & Authentication
+- Character advancement plugins
+- Combat system plugins
--- migrations/env.py
+++ migrations/env.py
@@ -1,27 +1,28 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
-
 from alembic import context
-from config import DATABASE_URI
-from data.models import Base
+
+from src.config import DATABASE_URI
+from src.data.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URI)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option('sqlalchemy.url', DATABASE_URI)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [Base.metadata]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -60,11 +61,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(DATABASE_URI)
 
     with connectable.connect() as connection:
         context.configure(
